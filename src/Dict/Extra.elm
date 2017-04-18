be1cf6616f4b0385eee2337c09afc86c3c329f28
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2017-04-18 10:54:46 +0200

    Ensure consistent formating and conventions.

--- src/Dict/Extra.elm
+++ src/Dict/Extra.elm
@@ -13,14 +13,21 @@ module Dict.Extra
 
 {-| Convenience functions for working with `Dict`
 
+
 # List operations
+
 @docs groupBy, fromListBy
 
+
 # Manipulation
+
 @docs removeWhen, removeMany, keepOnly, mapKeys, filterMap, invert
 
+
 # Find
+
 @docs find
+
 -}
 
 import Dict exposing (Dict)
@@ -34,6 +41,7 @@ Creates a `Dict` which maps the key to a list of matching elements.
     jack = {id=2, name="Jack"}
     jill = {id=1, name="Jill"}
     groupBy .id [mary, jack, jill] == Dict.fromList [(1, [mary, jill]), (2, [jack])]
+
 -}
 groupBy : (a -> comparable) -> List a -> Dict comparable (List a)
 groupBy keyfn list =
@@ -53,6 +61,7 @@ This can, for instance, be useful when constructing Dicts from a List of records
     jack = {id=2, name="Jack"}
     jill = {id=1, name="Jill"}
     fromListBy .id [mary, jack, jill] == Dict.fromList [(1, jack), (2, jill)]
+
 -}
 fromListBy : (a -> comparable) -> List a -> Dict comparable a
 fromListBy keyfn xs =
@@ -66,6 +75,7 @@ fromListBy keyfn xs =
 
     dict = Dict.fromList [("Mary", 1), ("Jack", 2), ("Jill", 1)]
     removeWhen (\_ v -> v == 1) dict == Dict.fromList [("Jack", 2)]
+
 -}
 removeWhen : (comparable -> v -> Bool) -> Dict comparable v -> Dict comparable v
 removeWhen pred dict =
@@ -76,6 +86,7 @@ removeWhen pred dict =
 
     dict = Dict.fromList [("Mary", 1), ("Jack", 2), ("Jill", 1)]
     removeMany (Set.fromList ["Mary", "Jill"]) dict == Dict.fromList [("Jack", 2)]
+
 -}
 removeMany : Set comparable -> Dict comparable v -> Dict comparable v
 removeMany set dict =
@@ -86,6 +97,7 @@ removeMany set dict =
 
     dict = Dict.fromList [("Mary", 1), ("Jack", 2), ("Jill", 1)]
     removeMany (Set.fromList ["Jack"]) dict == Dict.fromList [("Jack", 2)]
+
 -}
 keepOnly : Set comparable -> Dict comparable v -> Dict comparable v
 keepOnly set dict =
@@ -101,14 +113,16 @@ keepOnly set dict =
 
     mapKeys ((+) 1) (Dict.fromList [(5, "Jack"), (10, "Jill")])
     == Dict.fromList [(6, "Jack"), (11, "Jill")]
+
 -}
 mapKeys : (comparable -> comparable) -> Dict comparable v -> Dict comparable v
 mapKeys keyMapper dict =
-    let
-        addKey key value d =
-            Dict.insert (keyMapper key) value d
-    in
-        Dict.foldl addKey Dict.empty dict
+    Dict.foldl
+        (\k v acc ->
+            Dict.insert (keyMapper k) v acc
+        )
+        Dict.empty
+        dict
 
 
 {-| Apply a function that may or may not succeed to all entries in a dictionary,
@@ -128,6 +142,7 @@ but only keep the successes.
         ]
         |> filterMap isTeen
         == Dict.singleton 15 "JILL"
+
 -}
 filterMap : (comparable -> a -> Maybe b) -> Dict comparable a -> Dict comparable b
 filterMap f dict =
@@ -149,6 +164,7 @@ filterMap f dict =
     dict = Dict.fromList [("Jill", 5), ("Jack", 10)]
     inverted = Dict.fromList [(5, "Jill"), ("Jack", 10)]
     invert dict == inverted
+
 -}
 invert : Dict comparable1 comparable2 -> Dict comparable2 comparable1
 invert dict =
@@ -163,22 +179,23 @@ invert dict =
 {-| Find the first key/value pair that matches a predicate.
 
     dict = Dict.fromList [( 9, "Jill" ), ( 7, "Jill" )]
-    found = find (\key value -> value == "Jill") dict
+    found = find (\_ value -> value == "Jill") dict
     found == Just (7, "Jill")
 
 -}
 find : (comparable -> a -> Bool) -> Dict comparable a -> Maybe ( comparable, a )
-find predicate =
+find predicate dict =
     Dict.foldl
-        (\key value maybeTuple ->
-            case maybeTuple of
-                Just tuple ->
-                    Just tuple
+        (\k v acc ->
+            case acc of
+                Just _ ->
+                    acc
 
                 Nothing ->
-                    if predicate key value then
-                        Just ( key, value )
+                    if predicate k v then
+                        Just ( k, v )
                     else
                         Nothing
         )
         Nothing
+        dict
