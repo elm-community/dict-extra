1fce28a13e06dbae53256065b70d3ac4ad86fcc2
Ingar Almklov <ingara@gmail.com>
2017-07-31 20:25:20 +0200

    Add elm-test and elm-verify-examples as local deps (#18)
    Relying on global dependencies is error-prone, so here
    they are added as local deps.
    `elm-doc-test` has changed its name to `elm-verify-examples`
    and updated the test format. Code and dependency has been
    updated to reflect this.
    
    Since we have `elm-test` and `elm-verify-examples` as `npm`
    deps we can also use `npm` scripts to run them.

--- src/Dict/Extra.elm
+++ src/Dict/Extra.elm
@@ -40,10 +40,10 @@ import Set exposing (Set)
 {-| Takes a key-fn and a list.
 Creates a `Dict` which maps the key to a list of matching elements.
 
-    >>> import Dict
+    import Dict
 
-    >>> groupBy String.length [ "tree" , "apple" , "leaf" ]
-    Dict.fromList [ ( 4, [ "tree", "leaf" ] ), ( 5, [ "apple" ] ) ]
+    groupBy String.length [ "tree" , "apple" , "leaf" ]
+    --> Dict.fromList [ ( 4, [ "tree", "leaf" ] ), ( 5, [ "apple" ] ) ]
 
 -}
 groupBy : (a -> comparable) -> List a -> Dict comparable (List a)
@@ -59,8 +59,8 @@ groupBy keyfn list =
 {-| Create a dictionary from a list of values, by passing a function that can get a key from any such value.
 If the function does not return unique keys, earlier values are discarded.
 
-    >>> fromListBy String.length [ "tree" , "apple" , "leaf" ]
-    Dict.fromList [ ( 4, "leaf" ), ( 5, "apple" ) ]
+    fromListBy String.length [ "tree" , "apple" , "leaf" ]
+    --> Dict.fromList [ ( 4, "leaf" ), ( 5, "apple" ) ]
 
 -}
 fromListBy : (a -> comparable) -> List a -> Dict comparable a
@@ -76,10 +76,10 @@ duplicate keys. Create a dictionary from a list of pairs of keys and
 values, providing a function that is used to combine multiple values
 paired with the same key.
 
-    >>> fromListDedupe
-    ...     (\a b -> a ++ " " ++ b)
-    ...     [ ( "class", "menu" ), ( "width", "100%" ), ( "class", "big" ) ]
-    Dict.fromList [ ( "class", "menu big" ), ( "width", "100%" ) ]
+    fromListDedupe
+        (\a b -> a ++ " " ++ b)
+        [ ( "class", "menu" ), ( "width", "100%" ), ( "class", "big" ) ]
+    --> Dict.fromList [ ( "class", "menu big" ), ( "width", "100%" ) ]
 
 -}
 fromListDedupe : (a -> a -> a) -> List ( comparable, a ) -> Dict comparable a
@@ -92,8 +92,8 @@ fromListDedupe combine xs =
 
 {-| `fromListBy` and `fromListDedupe` rolled into one.
 
-    >>> fromListDedupeBy (\first second -> first) String.length [ "tree" , "apple" , "leaf" ]
-    Dict.fromList [ ( 4, "tree" ), ( 5, "apple" ) ]
+    fromListDedupeBy (\first second -> first) String.length [ "tree" , "apple" , "leaf" ]
+    --> Dict.fromList [ ( 4, "tree" ), ( 5, "apple" ) ]
 
 -}
 fromListDedupeBy : (a -> a -> a) -> (a -> comparable) -> List a -> Dict comparable a
@@ -106,9 +106,9 @@ fromListDedupeBy combine keyfn xs =
 
 {-| Remove elements which satisfies the predicate.
 
-    >>> Dict.fromList [ ( "Mary", 1 ), ( "Jack", 2 ), ( "Jill", 1 ) ]
-    ...     |> removeWhen (\_ value -> value == 1 )
-    Dict.fromList [ ( "Jack", 2 ) ]
+    Dict.fromList [ ( "Mary", 1 ), ( "Jack", 2 ), ( "Jill", 1 ) ]
+        |> removeWhen (\_ value -> value == 1 )
+    --> Dict.fromList [ ( "Jack", 2 ) ]
 
 -}
 removeWhen : (comparable -> v -> Bool) -> Dict comparable v -> Dict comparable v
@@ -118,11 +118,11 @@ removeWhen pred dict =
 
 {-| Remove a key-value pair if its key appears in the set.
 
-    >>> import Set
+    import Set
 
-    >>> Dict.fromList [ ( "Mary", 1 ), ( "Jack", 2 ), ( "Jill", 1 ) ]
-    ...     |> removeMany (Set.fromList [ "Mary", "Jill" ])
-    Dict.fromList [ ( "Jack", 2 ) ]
+    Dict.fromList [ ( "Mary", 1 ), ( "Jack", 2 ), ( "Jill", 1 ) ]
+        |> removeMany (Set.fromList [ "Mary", "Jill" ])
+    --> Dict.fromList [ ( "Jack", 2 ) ]
 
 -}
 removeMany : Set comparable -> Dict comparable v -> Dict comparable v
@@ -136,10 +136,10 @@ element at that key. The combining function is called with
 original element and the new element as arguments and
 returns the element to be inserted.
 
-    >>> Dict.fromList [ ( "expenses", 38.25 ), ( "assets", 100.85 ) ]
-    ...     |> insertDedupe (+) "expenses" 2.50
-    ...     |> insertDedupe (+) "liabilities" -2.50
-    Dict.fromList [ ( "expenses", 40.75 ), ( "assets", 100.85 ), ( "liabilities", -2.50 ) ]
+    Dict.fromList [ ( "expenses", 38.25 ), ( "assets", 100.85 ) ]
+        |> insertDedupe (+) "expenses" 2.50
+        |> insertDedupe (+) "liabilities" -2.50
+    --> Dict.fromList [ ( "expenses", 40.75 ), ( "assets", 100.85 ), ( "liabilities", -2.50 ) ]
 
 -}
 insertDedupe : (v -> v -> v) -> comparable -> v -> Dict comparable v -> Dict comparable v
@@ -158,9 +158,9 @@ insertDedupe combine key value dict =
 
 {-| Keep a key-value pair if its key appears in the set.
 
-    >>> Dict.fromList [ ( "Mary", 1 ), ( "Jack", 2 ), ( "Jill", 1 ) ]
-    ...     |> keepOnly (Set.fromList [ "Jack", "Jill" ])
-    Dict.fromList [ ( "Jack", 2 ), ( "Jill", 1 ) ]
+    Dict.fromList [ ( "Mary", 1 ), ( "Jack", 2 ), ( "Jill", 1 ) ]
+        |> keepOnly (Set.fromList [ "Jack", "Jill" ])
+    --> Dict.fromList [ ( "Jack", 2 ), ( "Jill", 1 ) ]
 
 -}
 keepOnly : Set comparable -> Dict comparable v -> Dict comparable v
@@ -175,13 +175,13 @@ keepOnly set dict =
 
 {-| Apply a function to all keys in a dictionary.
 
-    >>> Dict.fromList [ ( 5, "Jack" ), ( 10, "Jill" ) ]
-    ...     |> mapKeys (\x -> x + 1)
-    Dict.fromList [ ( 6, "Jack" ), ( 11, "Jill" ) ]
+    Dict.fromList [ ( 5, "Jack" ), ( 10, "Jill" ) ]
+        |> mapKeys (\x -> x + 1)
+    --> Dict.fromList [ ( 6, "Jack" ), ( 11, "Jill" ) ]
 
-    >>> Dict.fromList [ ( 5, "Jack" ), ( 10, "Jill" ) ]
-    ...     |> mapKeys toString
-    Dict.fromList [ ( "5", "Jack" ), ( "10", "Jill" ) ]
+    Dict.fromList [ ( 5, "Jack" ), ( 10, "Jill" ) ]
+        |> mapKeys toString
+    --> Dict.fromList [ ( "5", "Jack" ), ( "10", "Jill" ) ]
 
 -}
 mapKeys : (comparable -> comparable1) -> Dict comparable v -> Dict comparable1 v
@@ -197,16 +197,16 @@ mapKeys keyMapper dict =
 {-| Apply a function that may or may not succeed to all entries in a dictionary,
 but only keep the successes.
 
-    >>> let
-    ...     isTeen n a =
-    ...         if 13 <= n && n <= 19 then
-    ...             Just <| String.toUpper a
-    ...         else
-    ...             Nothing
-    ... in
-    ...     Dict.fromList [ ( 5, "Jack" ), ( 15, "Jill" ), ( 20, "Jones" ) ]
-    ...         |> filterMap isTeen
-    Dict.fromList [ ( 15, "JILL" ) ]
+    let
+        isTeen n a =
+            if 13 <= n && n <= 19 then
+                Just <| String.toUpper a
+            else
+                Nothing
+    in
+        Dict.fromList [ ( 5, "Jack" ), ( 15, "Jill" ), ( 20, "Jones" ) ]
+            |> filterMap isTeen
+    --> Dict.fromList [ ( 15, "JILL" ) ]
 
 -}
 filterMap : (comparable -> a -> Maybe b) -> Dict comparable a -> Dict comparable b
@@ -226,9 +226,9 @@ filterMap f dict =
 
 {-| Inverts the keys and values of an array.
 
-    >>> Dict.fromList [ ("key", "value")  ]
-    ...     |> invert
-    Dict.fromList [ ( "value", "key" ) ]
+    Dict.fromList [ ("key", "value")  ]
+        |> invert
+    --> Dict.fromList [ ( "value", "key" ) ]
 
 -}
 invert : Dict comparable1 comparable2 -> Dict comparable2 comparable1
@@ -243,13 +243,13 @@ invert dict =
 
 {-| Find the first key/value pair that matches a predicate.
 
-    >>> Dict.fromList [ ( 9, "Jill" ), ( 7, "Jill" ) ]
-    ...     |> find (\_ value -> value == "Jill")
-    Just ( 7, "Jill" )
+    Dict.fromList [ ( 9, "Jill" ), ( 7, "Jill" ) ]
+        |> find (\_ value -> value == "Jill")
+    --> Just ( 7, "Jill" )
 
-    >>> Dict.fromList [ ( 9, "Jill" ), ( 7, "Jill" ) ]
-    ...     |> find (\key _ -> key == 5)
-    Nothing
+    Dict.fromList [ ( 9, "Jill" ), ( 7, "Jill" ) ]
+        |> find (\key _ -> key == 5)
+    --> Nothing
 
 -}
 find : (comparable -> a -> Bool) -> Dict comparable a -> Maybe ( comparable, a )
