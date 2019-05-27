0fd78447b897f8a1cec8c654d4d41e1cef0eba3b
Sky Rose <sky.q.rose@gmail.com>
2019-05-26 22:41:32 -0400

    update tests to elm 19

--- src/Dict/Extra.elm
+++ src/Dict/Extra.elm
@@ -1,21 +1,8 @@
-module Dict.Extra
-    exposing
-        ( groupBy
-        , filterGroupBy
-        , fromListBy
-        , fromListDedupe
-        , fromListDedupeBy
-        , frequencies
-        , removeWhen
-        , removeMany
-        , insertDedupe
-        , keepOnly
-        , mapKeys
-        , filterMap
-        , invert
-        , any
-        , find
-        )
+module Dict.Extra exposing
+    ( groupBy, filterGroupBy, fromListBy, fromListDedupe, fromListDedupeBy, frequencies
+    , removeWhen, removeMany, keepOnly, insertDedupe, mapKeys, filterMap, invert
+    , any, find
+    )
 
 {-| Convenience functions for working with `Dict`
 
@@ -84,15 +71,15 @@ where key-fn returns `Nothing`
           }
         ]
     --> Dict.fromList
-        [ ( "Ford"
-          , [ { name = "Mary" , car = Just "Ford" } ]
-          )
-        , ( "Tesla"
-          , [ { name = "Jill" , car = Just "Tesla" }
-            , { name = "John" , car = Just "Tesla" }
-            ]
-          )
-        ]
+    --> [ ( "Ford"
+    -->   , [ { name = "Mary" , car = Just "Ford" } ]
+    -->   )
+    --> , ( "Tesla"
+    -->   , [ { name = "Jill" , car = Just "Tesla" }
+    -->     , { name = "John" , car = Just "Tesla" }
+    -->     ]
+    -->   )
+    --> ]
 
 -}
 filterGroupBy : (a -> Maybe comparable) -> List a -> Dict comparable (List a)
@@ -113,6 +100,8 @@ filterGroupBy keyfn list =
 {-| Create a dictionary from a list of values, by passing a function that can get a key from any such value.
 If the function does not return unique keys, earlier values are discarded.
 
+    import Dict
+
     fromListBy String.length [ "tree" , "apple" , "leaf" ]
     --> Dict.fromList [ ( 4, "leaf" ), ( 5, "apple" ) ]
 
@@ -130,6 +119,8 @@ duplicate keys. Create a dictionary from a list of pairs of keys and
 values, providing a function that is used to combine multiple values
 paired with the same key.
 
+    import Dict
+
     fromListDedupe
         (\a b -> a ++ " " ++ b)
         [ ( "class", "menu" ), ( "width", "100%" ), ( "class", "big" ) ]
@@ -146,6 +137,8 @@ fromListDedupe combine xs =
 
 {-| `fromListBy` and `fromListDedupe` rolled into one.
 
+    import Dict
+
     fromListDedupeBy (\first second -> first) String.length [ "tree" , "apple" , "leaf" ]
     --> Dict.fromList [ ( 4, "tree" ), ( 5, "apple" ) ]
 
@@ -159,6 +152,8 @@ fromListDedupeBy combine keyfn xs =
 
 
 {-| Count the number of occurences for each of the elements in the list.
+
+    import Dict
 
     frequencies [ "A", "B", "C", "B", "C", "B" ]
     --> Dict.fromList [ ( "A", 1 ), ( "B", 3 ), ( "C", 2 ) ]
@@ -179,6 +174,8 @@ frequencies list =
 
 {-| Remove elements which satisfies the predicate.
 
+    import Dict
+
     Dict.fromList [ ( "Mary", 1 ), ( "Jack", 2 ), ( "Jill", 1 ) ]
         |> removeWhen (\_ value -> value == 1 )
     --> Dict.fromList [ ( "Jack", 2 ) ]
@@ -191,6 +188,7 @@ removeWhen pred dict =
 
 {-| Remove a key-value pair if its key appears in the set.
 
+    import Dict
     import Set
 
     Dict.fromList [ ( "Mary", 1 ), ( "Jack", 2 ), ( "Jill", 1 ) ]
@@ -209,6 +207,8 @@ element at that key. The combining function is called with
 original element and the new element as arguments and
 returns the element to be inserted.
 
+    import Dict
+
     Dict.fromList [ ( "expenses", 38.25 ), ( "assets", 100.85 ) ]
         |> insertDedupe (+) "expenses" 2.50
         |> insertDedupe (+) "liabilities" -2.50
@@ -226,10 +226,13 @@ insertDedupe combine key value dict =
                 Nothing ->
                     Just value
     in
-        Dict.update key with dict
+    Dict.update key with dict
 
 
 {-| Keep a key-value pair if its key appears in the set.
+
+    import Dict
+    import Set
 
     Dict.fromList [ ( "Mary", 1 ), ( "Jack", 2 ), ( "Jill", 1 ) ]
         |> keepOnly (Set.fromList [ "Jack", "Jill" ])
@@ -248,12 +251,14 @@ keepOnly set dict =
 
 {-| Apply a function to all keys in a dictionary.
 
+    import Dict
+
     Dict.fromList [ ( 5, "Jack" ), ( 10, "Jill" ) ]
         |> mapKeys (\x -> x + 1)
     --> Dict.fromList [ ( 6, "Jack" ), ( 11, "Jill" ) ]
 
     Dict.fromList [ ( 5, "Jack" ), ( 10, "Jill" ) ]
-        |> mapKeys toString
+        |> mapKeys String.fromInt
     --> Dict.fromList [ ( "5", "Jack" ), ( "10", "Jill" ) ]
 
 -}
@@ -270,6 +275,8 @@ mapKeys keyMapper dict =
 {-| Apply a function that may or may not succeed to all entries in a dictionary,
 but only keep the successes.
 
+    import Dict
+
     let
         isTeen n a =
             if 13 <= n && n <= 19 then
@@ -277,8 +284,8 @@ but only keep the successes.
             else
                 Nothing
     in
-        Dict.fromList [ ( 5, "Jack" ), ( 15, "Jill" ), ( 20, "Jones" ) ]
-            |> filterMap isTeen
+    Dict.fromList [ ( 5, "Jack" ), ( 15, "Jill" ), ( 20, "Jones" ) ]
+        |> filterMap isTeen
     --> Dict.fromList [ ( 15, "JILL" ) ]
 
 -}
@@ -299,6 +306,8 @@ filterMap f dict =
 
 {-| Inverts the keys and values of an array.
 
+    import Dict
+
     Dict.fromList [ ("key", "value")  ]
         |> invert
     --> Dict.fromList [ ( "value", "key" ) ]
@@ -316,6 +325,8 @@ invert dict =
 
 {-| Determine if any key/value pair satisfies some test.
 
+    import Dict
+
     Dict.fromList [ ( 9, "Jill" ), ( 7, "Jill" ) ]
         |> any (\_ value -> value == "Jill")
     --> True
@@ -331,8 +342,10 @@ any predicate dict =
         (\k v acc ->
             if acc then
                 acc
+
             else if predicate k v then
                 True
+
             else
                 False
         )
@@ -341,6 +354,8 @@ any predicate dict =
 
 
 {-| Find the first key/value pair that matches a predicate.
+
+    import Dict
 
     Dict.fromList [ ( 9, "Jill" ), ( 7, "Jill" ) ]
         |> find (\_ value -> value == "Jill")
@@ -362,6 +377,7 @@ find predicate dict =
                 Nothing ->
                     if predicate k v then
                         Just ( k, v )
+
                     else
                         Nothing
         )
